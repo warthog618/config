33bd9cfce53a18b33b0385388258578026cd0b29
Kent Gibson <warthog618@gmail.com>
2017-03-17 22:37:58 +0800

    Add toml reader

---
+++ toml/toml.go
@@ -0,0 +1,45 @@
+// Copyright © 2017 Kent Gibson <warthog618@gmail.com>.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+// Package toml provides a TOML format reader for config.
+package toml
+
+import "github.com/pelletier/go-toml"
+
+// Reader provides the mapping from JSON to a config.Reader.
+type Reader struct {
+	config *toml.TomlTree
+}
+
+// Read returns the value for a given key and true if found, or
+// nil and false if not.
+func (r *Reader) Read(key string) (interface{}, bool) {
+	v := r.config.Get(key)
+	if v != nil {
+		if _, ok := v.(*toml.TomlTree); ok {
+			return nil, false
+		}
+		return v, true
+	}
+	return nil, false
+}
+
+// NewString returns a TOML reader that reads config from a string.
+func NewString(cfg string) (*Reader, error) {
+	config, err := toml.Load(cfg)
+	if err != nil {
+		return nil, err
+	}
+	return &Reader{config}, nil
+}
+
+// NewFile returns a JSON reader that reads config from a named file.
+func NewFile(filename string) (*Reader, error) {
+	config, err := toml.LoadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Reader{config}, nil
+}
